8b19db1e8e0bb40d03adae0b4d783a4359d4ec4e
root <root@instance-1.asia-northeast1-b.c.rosy-petal-295607.internal>
2020-11-16 08:53:44 +0000

    bug fixed login without logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,14 @@
 class SessionsController < ApplicationController
   
   def new
+     if logged_in? 
+
+       flash[:notice] = "must logout "
+       redirect_to root_path
+     end
     
   end
  
-  def online
-
-  end
-
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password]) 
